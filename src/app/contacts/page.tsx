4140849a03826d342d0ef1f07b6eda6f9f64d301
Khairul Islam <khairul15-4102@diu.edu.bd>
2025-03-28 23:47:41 +0600

    matha esh

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,25 +1,23 @@
+// /contacts/page.tsx
 import ContactsTable from "@/components/ContactsTable";
 import { Contacts as ContactsProps } from "../../types";
-// Async function to fetch customer data from the API
+
+// Async function to fetch contacts data from the API
 async function getContacts(): Promise<ContactsProps[]> {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/contacts`);
   const data = await res.json();
-  console.log(data.contacts);
+  // console.log(data.contacts);
   return data.contacts;
 }
 
-const Contacts = async () => {
+// Main ContactsPage component
+export default async function ContactsPage() {
   const contacts = await getContacts();
-  console.log("inside payments =====> ", contacts);
+
   return (
     <div>
-      <div>
-        <h1 className="text-xl font-bold mb-4">Contacts</h1>
-        {/* Pass the fetched customer data to the CustomersTable component as a prop */}
-        <ContactsTable initialContacts={contacts} />
-      </div>
+      <h1 className="text-xl font-bold mb-4">Contacts</h1>
+      <ContactsTable initialContacts={contacts} />
     </div>
   );
-};
-
-export default Contacts;
+}
